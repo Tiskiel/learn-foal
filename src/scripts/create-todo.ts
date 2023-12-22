358f97297826456733baf11314998c2973369428
Wanty Jean-François <wanty.jf@gmail.com>
2023-12-22 08:17:43 +0100

    Add todo script

--- src/scripts/create-todo.ts
+++ src/scripts/create-todo.ts
@@ -1,14 +1,28 @@
+import { Todo } from "../app/entities";
+import { dataSource } from "../db";
+
 export const schema = {
   additionalProperties: false,
   properties: {
-
+    text: {
+      type: "string",
+    },
   },
-  required: [
-
-  ],
-  type: 'object',
+  required: ["text"],
+  type: "object",
 };
 
 export async function main(args: any) {
+  await dataSource.initialize();
 
+  try {
+    const todo = new Todo();
+    todo.text = args.text;
+
+    console.log(await todo.save());
+  } catch (error) {
+    console.error(error);
+  } finally {
+    await dataSource.destroy();
+  }
 }
