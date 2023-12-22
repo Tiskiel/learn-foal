d34ac9307565814f1418a3ffec269d49dfed85ca
Wanty Jean-François <wanty.jf@gmail.com>
2023-12-22 08:12:16 +0100

    first commit

---
+++ src/migrations/1703228946258-migration.ts
@@ -0,0 +1,27 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class Migration1703228946258 implements MigrationInterface {
+    name = 'Migration1703228946258'
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`
+            CREATE TABLE "todo" (
+                "id" integer PRIMARY KEY AUTOINCREMENT NOT NULL,
+                "text" varchar NOT NULL
+            )
+        `);
+        await queryRunner.query(`
+            CREATE TABLE "user" ("id" integer PRIMARY KEY AUTOINCREMENT NOT NULL)
+        `);
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`
+            DROP TABLE "user"
+        `);
+        await queryRunner.query(`
+            DROP TABLE "todo"
+        `);
+    }
+
+}
