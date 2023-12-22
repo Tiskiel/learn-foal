8b9b2241b4a8ca52fa256bff5a371c9e4780ccef
Wanty Jean-François <wanty.jf@gmail.com>
2023-12-22 08:24:17 +0100

    Implement CRUD to do

--- src/app/controllers/api.controller.ts
+++ src/app/controllers/api.controller.ts
@@ -1,10 +1,39 @@
-import { Context, Get, HttpResponseOK } from '@foal/core';
+import {
+  Delete,
+  Get,
+  HttpResponseNotFound,
+  HttpResponseOK,
+  Post,
+} from "@foal/core";
+import { Todo } from "../entities";
+import { Context } from "mocha";
 
 export class ApiController {
-
-  @Get('/')
-  index(ctx: Context) {
-    return new HttpResponseOK('Hello world!');
+  @Get("/todos")
+  async getTodos() {
+    const todos = await Todo.find();
+    return new HttpResponseOK(todos);
   }
 
+  @Post("/todos/:id")
+  async postTodos(ctx: Context) {
+    const todo = new Todo();
+    todo.text = ctx.request.body.text;
+    await todo.save();
+
+    return new HttpResponseOK(todo);
+  }
+
+  @Delete("/todos/:id")
+  async deleteTodos(ctx: Context) {
+    const todo = await Todo.findOneBy({ id: ctx.request.params.id });
+
+    if (!todo) {
+      return new HttpResponseNotFound();
+    }
+
+    await todo.remove();
+
+    return new HttpResponseOK(todo);
+  }
 }
